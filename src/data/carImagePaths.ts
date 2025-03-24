ff430a07b56417dec97cd1ef6e8a99a546ff60b0
Mario Boboc <44845316+Thunder889@users.noreply.github.com>
2025-03-25 01:00:14 +0200

    a

---
+++ src/data/carImagePaths.ts
@@ -0,0 +1,6 @@
+// Define car image paths
+export const carImagePaths: Record<number, string> = {
+  1: '/images/cars/products/WWPassat.jpg',         // VW Passat
+  2: '/images/cars/products/PassatAlltrack.jpg',   // VW Passat Alltrack
+  3: '/images/cars/products/FordFocus.jpg',        // Ford Focus
+}; 
